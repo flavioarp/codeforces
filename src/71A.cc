a849a45021d68dd45783f9247cc3170b223f225d
flavioarp <flavioarp.k@gmail.com>
2025-07-06 21:55:39 -0300

    fix: Indentation

--- src/71A.cc
+++ src/71A.cc
@@ -1,15 +1,14 @@
 #include <iostream>
 
 int main(){
-	std::string s;
-	int a, n;
-	std::cin>>a;
-
-	while(a--){
-		std::cin>>s;
-		n = s.length();
-		if(n>10) std::cout<<s[0]<<n-2<<s[n-1]<<std::endl;
-		else std::cout<<s<<std::endl;
-	}
-	return 0;
+  std::string s;
+  int a, n;
+  std::cin>>a;
+  while(a--){
+    std::cin>>s;
+    n = s.length();
+    if(n>10) std::cout<<s[0]<<n-2<<s[n-1]<<std::endl;
+    else std::cout<<s<<std::endl;
+  }
+  return 0;
 }
