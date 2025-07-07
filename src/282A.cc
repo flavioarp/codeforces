beb8dcb6de92ff2918c1aed3338163578759a4da
flavioarp <flavioarp.k@gmail.com>
2025-07-06 21:53:09 -0300

    fix: Indentation

--- src/282A.cc
+++ src/282A.cc
@@ -1,16 +1,13 @@
-#include <string>
 #include <iostream>
  
 int main(){
   int n, x(0);
-	char s[101];
-	scanf("%d", &n);
-  
+  char s[101];
+  scanf("%d", &n);
   while(n--){
-		scanf("%s",s);
-		s[1] == '+' ? ++x : --x;
+    scanf("%s",s);
+    s[1] == '+' ? ++x : --x;
 	}
-  
-	std::cout<<x<<std::endl;
-	return 0;
+  std::cout<<x<<std::endl;
+  return 0;
 }
